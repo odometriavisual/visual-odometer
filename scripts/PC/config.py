dbec6b7d8f1920e0452ae6423c2b24f633811721
Daniel Santin <danielsantin@alunos.utfpr.edu.br>
2024-02-27 19:03:07 -0300

    Reorganizing the code

--- scripts/PC/config.py
+++ scripts/PC/config.py
@@ -1,15 +1,14 @@
-# Confurações da câmera
-camera_id = 0         # Defina o id da câmera (Padrão: 0)
-camera_exposure = -8  # Defina exposição da câmera (Padrão: -8)
+# Configurações de câmera
+camera_id = 0          # Defina o id da câmera (Padrão: 0, Valores Possíveis: Inteiros)
+camera_exposure = -8   # Defina exposição da câmera (Padrão: -8, Valores Possíveis: Inteiros)
 
 # Multiplicadores de deslocamento
-deltax_multiplier = 1 #Defina o multiplicador de deslocamento X
-deltay_multiplier = 1 #Defina o multiplicador de deslocamento Y
+deltax_multiplier = 1 # Defina o multiplicador de deslocamento X (Padrão: 1, Valores Possíveis: Inteiros)
+deltay_multiplier = 1 # Defina o multiplicador de deslocamento Y (Padrão: 1, Valores Possíveis: Inteiros)
+
+# Configuração de comunicação Serial
+usb_com_port = None  # Configure a porta de comunicação serial (Padrão: None, Valores Possíveis: String, Exemplo: "COM4")
 
 # Configurações de estimativa
-border_black_windowing = True  # Aplica o escurecimento nas bordas das imagens (Padrão: True)
-phase_windowing = None  # Aplica o janelamento no sinal final da fase (None, "initial", "central") (Padrão: None)
-
-# Configure uma porta COM, deixe None para tentar encontrar automaticamente
-usb_com_port = None  # Deve ser None ou uma string, exemplo: "COM4" (veja o arquivo "config_tools/usb_list.py") (Valor padrão: True)
-
+border_black_windowing = True # Aplica o escurecimento nas bordas das imagens (Padrão: True, Valores Possíveis: True, False)
+phase_windowing = None        # Aplica o janelamento no sinal final da fase (Padrão: None, Valores Possíveis: None, "initial", "central")
