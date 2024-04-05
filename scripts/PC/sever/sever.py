6144729690fa51716c9105da05b8370bd9c5b3b9
tatiprado <49042460+tatiprado@users.noreply.github.com>
2024-04-05 18:36:47 -0300

    web with IMU

--- scripts/PC/sever/sever.py
+++ scripts/PC/sever/sever.py
@@ -23,17 +23,20 @@ import config
 import matplotlib.pyplot as plt
 import numpy as np
 
+import serial
+import serial.tools.list_ports
+
+import time
+
+
 # Obtém o diretório atual do arquivo Python
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 # Caminho para o arquivo HTML
 html_file_path = os.path.join(dir_path, 'executar.html')
 
-resto_x = 0
-resto_y = 0
+ports = serial.tools.list_ports.comports()
 
-intxacumulado = 0
-intyacumulado = 0
 
 app = Flask(__name__)
 
@@ -41,6 +44,7 @@ lock_quat = threading.Lock()
 printar = False
 lista_dados = []  # este é o Y
 lista_dados2 = []  # este é o X
+lista_imu = []
 
 diretorio_atual = os.path.dirname(os.path.abspath(__file__))
 data_dir = os.path.join(diretorio_atual, "data")
@@ -63,135 +67,194 @@ counter = 0
 vid.set(cv2.CAP_PROP_AUTOFOCUS, 0)
 vid.set(cv2.CAP_PROP_FOCUS, counter)
 
+serial_encoder = 0
 
-# def atualizarPos(x,y):
-#     global resto_x, resto_y, arduino, intxacumulado, intyacumulado
-#
-#     x += resto_x
-#     y += resto_y
-#
-#     intx = int(x)
-#     inty = int(y)
-#
-#     text_to_send = f"0,{intx}, {inty} \n"
-#     arduino.write(text_to_send.encode())
-#     #_ = arduino.readline()
-#
-#     resto_x = x - intx
-#     resto_y = y - inty
-#
-# if config.usb_com_port is None:
-#     try:
-#         print("Iniciando setup automático de comunicação Serial")
-#         serial_port_list = serial.tools.list_ports.comports()
-#         serial_port_list_size = len(serial_port_list)
-#         if (serial_port_list_size == 0):
-#             print ("Não foi detectado nenhuma comunicação serial compatível")
-#         elif (serial_port_list_size > 1):
-#             warnings.warn("ATENÇÃO - Foram encontradas mais de uma porta serial, o código exercutaa apenas com uma delas")
-#         selected_port = sorted(serial_port_list)[0]
-#         arduino = serial.Serial(port=selected_port.name, baudrate=115200, timeout=1)
-#         state = arduino.read()
-#         print(f"Porta {selected_port.name} conectada")
-#     except:
-#         print("Erro na conexão da comunicação serial, verifique se o encoder está conectado")
-#         exit()
-# else:
-#     try:
-#         arduino = serial.Serial(port=config.usb_com_port, baudrate=115200, timeout=1)
-#     except:
-#         print("Erro na conexão da comunicação serial, é recomendado alterar a variável usb_com_port no config.py para None")
-#         exit()
+gyroData = [0,0,0,0]
 
+# --- Funções reservadas para envio e armazenamento de dados --- #
 
-# - Fim do setup Serial - #
+def checkSerialInput():
+    global gyroData, serial_giroscopio
+    if (serial_giroscopio.in_waiting > 0):
+        ser_line = serial_giroscopio.readline().decode()
+        gyroData = [float(x) for x in ser_line.split(",")] #list  quaternium to biallhole
+        serial_giroscopio.flush()
+
+def serialSendEncoder(x, y):
+    global resto_x, resto_y, serial_encoder, intxacumulado, intyacumulado, serial_encoder
+
+    x += resto_x
+    y += resto_y
+
+    intx = int(x)
+    inty = int(y)
+
+    text_to_send = f"0,{intx}, {inty} \n"
+    serial_encoder.write(text_to_send.encode())
+
+    resto_x = x - intx
+    resto_y = y - inty
+
+# ----- Configuração de comunicação Serial ----- #
+
+#Comentado para remover dados seriais
 
 def minha_thread():
     global printar
     global vid
-    total_deltax = 0
-    total_deltay = 0
+    global resto_x
+    global resto_y
+    global lista_imu
+    global intxacumulado
+    global intyacumulado
+    global serial_encoder
+    global serial_giroscopio
+
     totalx = 0
     totaly = 0
-    while True:
-        print('Pegando acesso a camera, isso pode demorar um pouco...')
+    giroscop = 0
+    # --- definição de variáveis necessárias --- #
 
+    total_deltax = 0
+    total_deltay = 0
+
+    M = None
+    N = None
+    start_time = 0
+
+    resto_x = 0
+    resto_y = 0
+
+    intxacumulado = 0
+    intyacumulado = 0
+
+    print("")
+    for port in ports:
+        print(port)
+        if port.serial_number == "5698028262":
+            print("Iniciando conecção com o modulo do giroscópio")
+            serial_giroscopio = serial.Serial(port=port.device, baudrate=9600, timeout=1)
+            time.sleep(0.3)
+            serial_giroscopio.setRTS(True)
+            time.sleep(0.3)
+            serial_giroscopio.setRTS(False)
+            time.sleep(0.3)
+        else:
+            serial_encoder = serial.Serial(port=port.device, baudrate=115200, timeout=1)
+            time.sleep(0.3)
+            serial_encoder.setRTS(True)
+            time.sleep(0.3)
+            serial_encoder.setRTS(False)
+            time.sleep(0.3)
+            print("Iniciando comunicação com o modulo pulsador")
+            time.sleep(0.6)
+
+    print("Testando comunicação serial: encoder")
+    _ = serial_encoder.read()
+    print("Testando comunicação serial: giroscópio server")
+    _ = serial_giroscopio.read()
+    print("Comunicação serial OK")
+    print("")
+
+    # -- Inicio da configuração da câmera --- #
+
+    print('Pegando acesso a camera, isso pode demorar um pouco...')
+    vid = cv2.VideoCapture(config.camera_id)
+
+    ret, frame = vid.read()
+    img_array = cv2_to_nparray_grayscale(frame)
+    if config.camera_exposure != None:
+        print("Definindo exposição da câmera")
+        vid.set(cv2.CAP_PROP_EXPOSURE, config.camera_exposure)
+
+    # Abre o arquivo HTML no navegador padrão
+    webbrowser.open('file://' + html_file_path)
+
+    frame_num = -10
+    while True:
         try:
+            # Comentado para remover serial
+            checkSerialInput()
+
             ret, frame = vid.read()
             img_array = cv2_to_nparray_grayscale(frame)
-        except:
-            print("Não foi possível conectar a câmera, altere o id da camera no config.py")
+            img_windowed = apply_border_windowing_on_image(img_array, config.border_windowing_method)
+            img_processed = image_preprocessing(img_array)
+            if frame_num > 0:
+                if (M == None):
+                    print("Script iniciado")
+                    start_time = time.time()
+                    M, N = img_array.shape
+                deltax, deltay = optimized_svd_method(img_processed, img_processed_old, M, N,
+                                                      phase_windowing=config.phase_windowing)
+                multiplied_deltax = config.deltax_multiplier * deltax
+                multiplied_deltay = config.deltay_multiplier * deltay
+
+                # Comentado para remover serial
+                serialSendEncoder(multiplied_deltax,
+                                  multiplied_deltay)  # <- Envia informações de deslocamento para o modulo pulsador
+
+                total_deltax = total_deltax + multiplied_deltax
+                total_deltay = total_deltay + multiplied_deltay
+
+                # Exemplo de array para ser salvo:
+                array_to_save = [time.time(), gyroData, deltax, deltay]
+                print(array_to_save)
+
+                # salvando x e y para o web
+                # Recomendo limitar o salvamento a um intervalo. Mas é só uma sugestão mesmo.
+
+                totaly = round(total_deltay, 2)
+                totalx = round(total_deltax, 2)
+
+                lock_quat.acquire()
+                if printar:
+                    lista_dados2.append(totalx)
+                    lista_dados.append(totaly)
+                    lista_imu.append(gyroData)
+                lock_quat.release()
+
+            frame_num = frame_num + 1
+            img_processed_old = img_processed
+
+        except KeyboardInterrupt:
+
+            # Fechando portas Seriais
+            serial_giroscopio.setRTS(True)
+            serial_encoder.setRTS(True)
+            time.sleep(0.3)
+            serial_giroscopio.setRTS(False)
+            serial_encoder.setRTS(False)
+            time.sleep(0.3)
+            serial_giroscopio.close()
+            serial_encoder.close()
+            # -----------------------#
+
+            vid.release()
+
+            passed_time = (time.time() - start_time)
+            print("--- %s seconds ---" % passed_time)
+            print("--- %s  frames ---" % frame_num)
+
+            fps = frame_num / passed_time
+            print("--- %s     fps ---" % fps)
+            print("")
+            print(f"Total deltax: {total_deltax}")
+            print(f"Total deltay: {total_deltay}")
+
             exit()
-
-        if config.camera_exposure != None:
-            print("Definindo exposição da câmera")
-            vid.set(cv2.CAP_PROP_EXPOSURE, config.camera_exposure)
-
-        frame_num = -10  # Definido para negativo, o frame será contabilizado apenas após a décima imagem
-
-        M = None
-        N = None
-        start_time = 0
-
-        # Abre o arquivo HTML no navegador padrão
-        webbrowser.open('file://' + html_file_path)
-
-        while True:
-            try:
-                ret, frame = vid.read()
-                img_array = cv2_to_nparray_grayscale(frame)
-                img_windowed = apply_border_windowing_on_image(img_array, config.border_windowing_method)
-                img_processed = image_preprocessing(img_array)
-                if frame_num > 0:
-                    if (M == None):
-                        print("Script iniciado")
-                        start_time = time.time()
-                        M, N = img_array.shape
-                    deltax, deltay = optimized_svd_method(img_processed, img_processed_old, M, N,
-                                                          phase_windowing=config.phase_windowing)
-                    multiplied_deltax = config.deltax_multiplier * deltax
-                    multiplied_deltay = config.deltay_multiplier * deltay
-                    # atualizarPos(multiplied_deltax,multiplied_deltay)
-                    total_deltax = total_deltax + (multiplied_deltax)
-                    total_deltay = total_deltay + (multiplied_deltay)
-
-                    print(
-                        f"Frame:  {frame_num:>3.2f}, delta:[{deltax:>5.2f},{deltay:>5.2f}], total_delta:[{total_deltax:>5.2f}, {total_deltay:>5.2f}]")
-
-                    totaly = round(total_deltay, 2)
-                    totalx = round(total_deltax, 2)
-                    lock_quat.acquire()
-                    if printar:
-                        lista_dados2.append(totalx)
-                        lista_dados.append(totaly)
-                    lock_quat.release()
-
-                frame_num = frame_num + 1
-                img_processed_old = img_processed
-            except KeyboardInterrupt:
-                vid.release()
-
-                passed_time = (time.time() - start_time)
-                print("--- %s seconds ---" % passed_time)
-                print("--- %s  frames ---" % frame_num)
-
-                fps = frame_num / passed_time
-                print("--- %s     fps ---" % fps)
-                print("")
-                print(f"Total deltax: {total_deltax}")
-                print(f"Total deltay: {total_deltay}")
-
-                exit()
+        except Exception as exc:
+            print("Erro:", exc)
 
 
 def salvar_dados_arquivo():
-    global lista_dados, lista_dados2
+    global lista_dados, lista_dados2, lista_imu
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")  # Cria um timestamp único
     arquivo_dados = os.path.join(data_dir, f"dados_{timestamp}.txt")  # Nome do arquivo com timestamp
     with open(arquivo_dados, "w") as arquivo:
-        for x, y in zip(lista_dados, lista_dados2):
+        for x, y, z in zip(lista_dados, lista_dados2, lista_imu):
             arquivo.write(
-                f"{x} | {y}\n")  # Escreve os dados x e y em uma linha, separados por um espaço e com uma quebra de linha no final
+                f"{x}|{y}|{z}\n")  # Escreve os dados x e y em uma linha, separados por um espaço e com uma quebra de linha no final
 
 
 @app.route('/iniciar', methods=["GET", "POST"])
@@ -201,9 +264,9 @@ def iniciar():
     printar = True
     return '<div style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh;"><form action="/finalizar" method="post"><button type="submit" style="width: 150px; height: 50px;">stop</button></form></div>'
 
-
 @app.route('/finalizar', methods=["GET", "POST"])
 def finalizar():
+    print("teste")
     global printar
     global lista_dados
     salvar_dados_arquivo()
