2e36ab584d0ea77e106b8f4908f8995b4f8455cf
Daniel Santin <danielsantin@alunos.utfpr.edu.br>
2024-06-10 10:35:20 -0300

    Modifying to add a smooth start and end

--- scripts/arduino/step_controller/step_controller.ino
+++ scripts/arduino/step_controller/step_controller.ino
@@ -5,10 +5,15 @@ int steps_per_second = 100;
 int current_position = 0;
 int step_direction = 1;
 int target_position = 0;
+int total_steps = 0;
+int steps_to_go = 0;
 float last_step_time = 0;
-float step_interval = 1000/steps_per_second;
+float step_interval = 1000000.0 / steps_per_second;
+float desired_step_interval = step_interval
 bool is_moving = false;
-
+int acceleration_steps = 10; // Número de passos para aceleração/desaceleração
+int initial_slow_steps = 0;
+int final_slow_steps = 0;
 
 void setup() {
   pinMode(DIR_PIN, OUTPUT);
@@ -23,42 +28,44 @@ void loop() {
     char *token = strtok(const_cast<char*>(incoming_text.c_str()), ",");
     if (token != NULL) {
       target_position = atoi(token);
+      total_steps = abs(target_position - current_position);
+      initial_slow_steps = total_steps * 0.1;
+      final_slow_steps = total_steps * 0.1;
+      steps_to_go = total_steps;
       token = strtok(NULL, ",");
       if (token != NULL) {
-        //Serial.print("Token: ");
-        //Serial.println(token);
-        //Serial.print("ATOF TOKEN: ");
-        //Serial.println(atof(token));
-        //Serial.print("Step: ");
-
-        step_interval = 1000000/atof(token);
-        //Serial.println(step_interval);
+        desired_step_interval = 1000000.0 / atof(token);
       }
     }
-    if (target_position - current_position > 0) {
-      step_direction = 1;
-      digitalWrite(DIR_PIN, LOW);
-      is_moving = true;
-    } else if (target_position - current_position < 0) {
-      step_direction = -1;
-      digitalWrite(DIR_PIN, HIGH);
+    if (total_steps > 0) {
+      step_direction = (target_position > current_position) ? 1 : -1;
+      digitalWrite(DIR_PIN, (step_direction == 1) ? LOW : HIGH);
       is_moving = true;
     }
   }
-  while (is_moving == true){
-    if (current_position != target_position) {
+
+  while (is_moving) {
+    if (steps_to_go > 0) {
       float current_time = micros();
       if (current_time - last_step_time >= step_interval) {
         digitalWrite(STEP_PIN, HIGH);
         digitalWrite(STEP_PIN, LOW);
         last_step_time = current_time;
-        current_position = current_position + step_direction;
+        current_position += step_direction;
+        steps_to_go--;
+
+        // Ajuste da velocidade
+        if (steps_to_go > total_steps - initial_slow_steps || steps_to_go < final_slow_steps) {
+          // Acelerando ou desacelerando
+          step_interval = map(steps_to_go, total_steps - initial_slow_steps, total_steps, 200000, desired_step_interval);
+        } else {
+          // Velocidade constante
+          step_interval = step_interval;
+        }
       }
-    } else if (is_moving == true) {
+    } else {
       Serial.println(".");
       is_moving = false;
     }
   }
 }
-
-
