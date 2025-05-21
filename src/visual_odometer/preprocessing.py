df852f51aa16fb46056f92ca323933c1ad6c9a3d
Demarky <danielsantin@alunos.utfpr.edu.br>
2025-05-21 16:48:48 -0300

    Changing the "Match" method to if and elses for compatibility

--- src/visual_odometer/preprocessing.py
+++ src/visual_odometer/preprocessing.py
@@ -5,16 +5,15 @@ from .dsp import *
 
 
 def apply_spatial_window(img: ndarray, method: str, params: dict):
-    match method:
-        case "blackman_harris":
-            return apply_blackman_harris_window(img, params['a0'], params['a1'], params['a2'], params['a3'])
-        case "raised_cosine":
-            return apply_raised_cosine_window(img)
-        case "":
-            return img
-        case _:
-            print(f'Atenção: tentando aplicar o método de janelamento de imagem {method}, mas ele não está implementado.')
-            return img
+    if method == "blackman_harris":
+        return apply_blackman_harris_window(img, params['a0'], params['a1'], params['a2'], params['a3'])
+    elif method == "raised_cosine":
+        return apply_raised_cosine_window(img)
+    elif method == "":
+        return img
+    else:
+        print(f'Atenção: tentando aplicar o método de janelamento de imagem {method}, mas ele não está implementado.')
+        return img
 
 
 def apply_downsampling(img: np.ndarray, method: str, params: dict):
@@ -22,29 +21,27 @@ def apply_downsampling(img: np.ndarray, method: str, params: dict):
     newsize = int(img.shape[0] / factor), int(img.shape[1] / factor)
     img_pil = Image.fromarray(img)
 
-    match method:
-        case "NN":
-            return np.array(img_pil.resize(newsize, Image.NEAREST))
-        case "bilinear":
-            return np.array(img_pil.resize(newsize, Image.BILINEAR))
-        case "bicubic":
-            return np.array(img_pil.resize(newsize, Image.BICUBIC))
-        case "":
-            return img
-        case _:
-            print(f'Atenção: tentando aplicar o método de downsampling {method}, mas ele não está implementado.')
-            return img
+    if method == "NN":
+        return np.array(img_pil.resize(newsize, Image.NEAREST))
+    elif method == "bilinear":
+        return np.array(img_pil.resize(newsize, Image.BILINEAR))
+    elif method == "bicubic":
+        return np.array(img_pil.resize(newsize, Image.BICUBIC))
+    elif method == "":
+        return img
+    else:
+        print(f'Atenção: tentando aplicar o método de downsampling {method}, mas ele não está implementado.')
+        return img
 
 
 def apply_frequency_window(spectrum: np.ndarray, method: str, params: dict):
-    match method:
-        case "Stone_et_al_2001":
-            return ideal_lowpass(spectrum, params["factor"])
-        case "":
-            return spectrum
-        case _:
-            print(f'Atenção: tentando aplicar o método {method}, mas ele não está implementado.')
-            return spectrum
+    if method == "Stone_et_al_2001":
+        return ideal_lowpass(spectrum, params["factor"])
+    elif method == "":
+        return spectrum
+    else:
+        print(f'Atenção: tentando aplicar o método {method}, mas ele não está implementado.')
+        return spectrum
 
 
 # Function which applies all the preprocessing:
