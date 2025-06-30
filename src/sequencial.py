adff74cb89b08b73382d7e0740cf4aef8b8cf857
Fernando <caetanof@alunos.utfpr.edu.br>
2025-06-30 14:21:51 -0300

    add non repeated sequential test

--- src/sequencial.py
+++ src/sequencial.py
@@ -1,63 +1,43 @@
-"""
-    Example for processing the displacements between frames in an image stream.
-"""
-
 from visual_odometer import VisualOdometer
 import time
+from glob import glob
+from os import path
 from PIL import Image, ImageOps
-import numpy as np
 
-use_gpu = False
 try:
-    import cupy as cp
-except:
-    use_gpu = False
-    pass
+    import cupy as xp
+except Exception:
+    import numpy as xp
 
 def load_img(filename):
     img_array_rgb = Image.open(filename)
     img_grayscale = ImageOps.grayscale(img_array_rgb)
     return img_grayscale
 
-grayscale_img0 = load_img('../datasets/dario_320x240/img.png')  # image at t = t₀
-grayscale_img1 = load_img('../datasets/dario_320x240/img_translated.png') # image at t = t₀ + Δt
+img_stream = [(path.split(img_path)[1], xp.asarray(load_img(img_path))) for img_path in glob('../datasets/11_20250618T152749_320x240/*.jpg')]
 
-if use_gpu:
-    img0 = cp.asarray(grayscale_img0)
-    img1 = cp.asarray(grayscale_img1)
-else:
-    img0 = np.asarray(grayscale_img0)
-    img1 = np.asarray(grayscale_img1)
-
-stream_size = 100
-img_stream = [img0, img1] * stream_size
-
-odometer = VisualOdometer(img_shape=(640, 480))
-odometer.feed_image(img0)
+odometer = VisualOdometer(img_shape=img_stream[0][1].shape)
+odometer.feed_image(img_stream[0][1])
 odometer.get_displacement()
-odometer.feed_image(img1)
+odometer.feed_image(img_stream[1][1])
 odometer.get_displacement()
-
-time.sleep(1)
 
 t0 = time.time()
 
-for img in img_stream:
-    #load_as_cpu_img('./img.png')
+for img_path, img in img_stream:
+    ti0 = time.time()
     odometer.feed_image(img)
-    print(odometer.get_displacement())
-delta_t = time.time() - t0
+    odometer.get_displacement()
+    ti1 = time.time()
+    print(f'{img_path}: dt={(ti1-ti0)*1000:.3f}ms')
+
+t1 = time.time()
+
+delta_t = t1 - t0
 
 print(f"""
 Number of frames: {len(img_stream)}
 Processed frames: {odometer.number_of_displacements} out of {len(img_stream)}
-Real FPS = {odometer.number_of_displacements / delta_t:.2f}."
-      """)
-
-
-
-
-
-
-
+Real FPS = {odometer.number_of_displacements / delta_t:.2f}"
+""")
 
---
+++ src/sequencial_repeated.py
@@ -0,0 +1,59 @@
+"""
+    Example for processing the displacements between frames in an image stream.
+"""
+
+from visual_odometer import VisualOdometer
+import time
+from glob import glob
+from random import randint
+from PIL import Image, ImageOps
+import numpy as np
+
+use_gpu = False
+try:
+    import cupy as cp
+except Exception:
+    use_gpu = False
+    pass
+
+def load_img(filename):
+    img_array_rgb = Image.open(filename)
+    img_grayscale = ImageOps.grayscale(img_array_rgb)
+    return img_grayscale
+
+filenames = glob('../datasets/11_20250618T152749_320x240/*.jpg')
+i = randint(1, len(filenames))
+grayscale_img0 = load_img(filenames[i])  # image at t = t₀
+grayscale_img1 = load_img(filenames[i+1]) # image at t = t₀ + Δt
+
+if use_gpu:
+    img0 = cp.asarray(grayscale_img0)
+    img1 = cp.asarray(grayscale_img1)
+else:
+    img0 = np.asarray(grayscale_img0)
+    img1 = np.asarray(grayscale_img1)
+
+stream_size = 100
+img_stream = [img0, img1] * stream_size
+
+odometer = VisualOdometer(img_shape=img0.shape)
+odometer.feed_image(img0)
+odometer.get_displacement()
+odometer.feed_image(img1)
+odometer.get_displacement()
+
+time.sleep(1)
+
+t0 = time.time()
+
+for img in img_stream:
+    #load_as_cpu_img('./img.png')
+    odometer.feed_image(img)
+    print(odometer.get_displacement())
+delta_t = time.time() - t0
+
+print(f"""
+Number of frames: {len(img_stream)}
+Processed frames: {odometer.number_of_displacements} out of {len(img_stream)}
+Real FPS = {odometer.number_of_displacements / delta_t:.2f}."
+      """)
