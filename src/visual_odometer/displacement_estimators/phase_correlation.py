669a466d954f50a670c3dd777ecba3615743ba56
daniel santin <danielsantin@alunos.utfpr.edu.br>
2025-07-14 14:14:13 -0300

    Removing peak detection from the phase correlation method, because it didn't work very well.

--- src/visual_odometer/displacement_estimators/phase_correlation.py
+++ src/visual_odometer/displacement_estimators/phase_correlation.py
@@ -4,28 +4,8 @@ try:
 except ImportError:
     cp = None
 
-def subpixel_peak_position(corr_abs, peak, xp):
-    y, x = peak
-
-    def parabolic(f, x):
-        """Retorna o deslocamento subpixel do vértice da parábola definida por três pontos."""
-        if x <= 0 or x >= f.shape[0] - 1:
-            return 0.0  # borda, não é possível interpolar
-        denom = (f[x-1] - 2*f[x] + f[x+1])
-        if denom == 0:
-            return 0.0
-        return 0.5 * (f[x-1] - f[x+1]) / denom
-
-    dx = dy = 0.0
-    if 1 <= x < corr_abs.shape[1] - 1:
-        dx = parabolic(corr_abs[y, :], x)
-    if 1 <= y < corr_abs.shape[0] - 1:
-        dy = parabolic(corr_abs[:, x], y)
-
-    return x + dx, y + dy
-
 def phase_correlation_method(fft_beg, fft_end, use_gpu=False):
-    xp = cp if use_gpu and cp else np
+    xp = cp if use_gpu and 'cp' in globals() else np
 
     # Cross-power spectrum
     R = fft_end * xp.conj(fft_beg)
@@ -36,15 +16,15 @@ def phase_correlation_method(fft_beg, fft_end, use_gpu=False):
     corr = xp.fft.fftshift(corr)
     corr_abs = xp.abs(corr)
 
-    # Pico
+    # Pico (sem subpixel)
     max_idx = xp.unravel_index(xp.argmax(corr_abs), corr.shape)
-    sub_x, sub_y = subpixel_peak_position(corr_abs, max_idx, xp)
 
     # Centro
     mid_y, mid_x = corr.shape[0] // 2, corr.shape[1] // 2
 
-    # Deslocamento
-    dx = sub_x - mid_x
-    dy = sub_y - mid_y
+    # Deslocamento inteiro
+    dx = max_idx[1] - mid_x
+    dy = max_idx[0] - mid_y
 
     return float(dx), float(dy)
+
