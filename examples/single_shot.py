5f7173907fc6b058af151a430302475ad5599300
Fernando <caetanof@alunos.utfpr.edu.br>
2025-10-29 16:03:22 -0300

    remove old methods related to DEFAULT_CONFIG

--- examples/single_shot.py
+++ examples/single_shot.py
@@ -16,9 +16,7 @@ img0 = np.asarray(load_img('../datasets/dario_320x240/img.png')) # image at t = 
 img1 = np.asarray(load_img('../datasets/dario_320x240/img_translated.png')) # image at t = t₀ + Δt
 
 odometer = VisualOdometer(img_shape=img0.shape)
-odometer.save_config('./')
 
-odometer.calibrate(new_xres=1.0, new_yres=1.0)
 t0 = time.time()
 dx, dy = odometer.estimate_displacement_between(img0, img1)
 dt = (time.time() - t0) * 1000
