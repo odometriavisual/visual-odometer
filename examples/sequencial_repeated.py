073705d8f0a4e09f6c55a7d964f5a557146b5418
Fernando <caetanof@alunos.utfpr.edu.br>
2025-10-29 14:51:21 -0300

    remove gpu code from examples

--- examples/sequencial_repeated.py
+++ examples/sequencial_repeated.py
@@ -9,13 +9,6 @@ from random import randint
 from PIL import Image, ImageOps
 import numpy as np
 
-use_gpu = False
-try:
-    import cupy as cp
-except Exception:
-    use_gpu = False
-    pass
-
 def load_img(filename):
     img_array_rgb = Image.open(filename)
     img_grayscale = ImageOps.grayscale(img_array_rgb)
@@ -26,12 +19,8 @@ i = randint(0, len(filenames)-2)
 grayscale_img0 = load_img(filenames[i])  # image at t = t₀
 grayscale_img1 = load_img(filenames[i+1]) # image at t = t₀ + Δt
 
-if use_gpu:
-    img0 = cp.asarray(grayscale_img0)
-    img1 = cp.asarray(grayscale_img1)
-else:
-    img0 = np.asarray(grayscale_img0)
-    img1 = np.asarray(grayscale_img1)
+img0 = np.asarray(grayscale_img0)
+img1 = np.asarray(grayscale_img1)
 
 stream_size = 100
 img_stream = [img0, img1] * stream_size
@@ -47,7 +36,6 @@ time.sleep(1)
 t0 = time.time()
 
 for img in img_stream:
-    #load_as_cpu_img('./img.png')
     odometer.feed_image(img)
     print(odometer.get_displacement())
 delta_t = time.time() - t0
